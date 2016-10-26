d363946543e36d6b659907838c21797c55376cc3
Juanse Gimenez <juanseprogressive@gmail.com>
2016-10-26 11:07:49 -0300

    Create new controller for default views home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 	
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/faq'
+
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
