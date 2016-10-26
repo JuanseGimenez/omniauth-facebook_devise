21579c24fd7dfa94e48bcb41d19e68c2e30fd18d
Juanse Gimenez <juanseprogressive@gmail.com>
2016-10-26 10:47:25 -0300

    Add your client ID and SECRET for API facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -248,6 +248,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, "804688133004338", "63bf5ebe8e0fd04c96a96a20a9e88bb4"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
